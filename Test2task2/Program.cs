a3098ef1407b59b4a1f94235df365755436ca945
alexdesty <alex.desty@gmail.com>
2025-02-16 20:26:47 +0300

    Add 1 and 2 task

---
+++ Test2task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Test2task2.Extensions;
+
+namespace Test2task2;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var num = -43874;
+        Console.WriteLine(num.IntToStr());
+    }
+}
+
